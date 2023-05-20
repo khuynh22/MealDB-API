1e032421e94cc0376ec4431b2754e6223b524b3a
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-20 16:11:24 -0500

    Initial Commit

---
+++ MealDB-API/MealDB_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDB_APIApp.swift
+//  MealDB-API
+//
+//  Created by Khang Nguyen Huynh on 5/20/23.
+//
+
+import SwiftUI
+
+@main
+struct MealDB_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealDB-API/Support/removeEmpty.swift
@@ -0,0 +1,8 @@
+//
+//  removeEmpty.swift
+//  MealDB-API
+//
+//  Created by Khang Nguyen Huynh on 5/20/23.
+//
+
+import Foundation
