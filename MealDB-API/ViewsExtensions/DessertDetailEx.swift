f1d57edd7d9179498854262f7a490b233d9745fc
Tim Huynh <57774658+khuynh22@users.noreply.github.com>
2023-05-21 21:27:47 -0500

    Finish Extension

--- MealDB-API/ViewsExtensions/DessertDetailEx.swift
+++ MealDB-API/ViewsExtensions/DessertDetailEx.swift
@@ -22,45 +22,45 @@ extension DessertDetailView {
             self.meal = meal
             self.instructions = ""
             self.ingredients = [IngredientModel]()
-            
-            func fetchMealDetails() async {
-                func handleSuccess(_ items: NetworkResponse) -> Void {
-                    if let details = items.meals.first {
-                        setInstructions(using: details)
-                        setIngredients(using: details)
-                    }
+        }
+        func fetchMealDetails() async {
+            func handleSuccess(_ items: NetworkResponseResult) -> Void {
+                if let details = items.meals.first {
+                    setInstructions(using: details)
+                    setIngredients(using: details)
                 }
-                
-                await fetch(from: detailsURL, onSuccess: handleSuccess)
             }
-            
-            private func setInstructions(using details: NetworkResponse) {
-                self.strInstructions = details.strInstructions
-            }
-            
-            private func setIngredients(using details: NetworkResponse) {
-                let ingredMeasurements = [
-                    IngredientModel(name: details.strIngredient1, quantity: details.strMeasure1),
-                    IngredientModel(name: details.strIngredient2, quantity: details.strMeasure2),
-                    IngredientModel(name: details.strIngredient3, quantity: details.strMeasure3),
-                    IngredientModel(name: details.strIngredient4, quantity: details.strMeasure4),
-                    IngredientModel(name: details.strIngredient5, quantity: details.strMeasure5),
-                    IngredientModel(name: details.strIngredient6, quantity: details.strMeasure6),
-                    IngredientModel(name: details.strIngredient7, quantity: details.strMeasure7),
-                    IngredientModel(name: details.strIngredient8, quantity: details.strMeasure8),
-                    IngredientModel(name: details.strIngredient9, quantity: details.strMeasure9),
-                    IngredientModel(name: details.strIngredient10, quantity: details.strMeasure10),
-                    IngredientModel(name: details.strIngredient11, quantity: details.strMeasure11),
-                    IngredientModel(name: details.strIngredient12, quantity: details.strMeasure12),
-                    IngredientModel(name: details.strIngredient13, quantity: details.strMeasure13),
-                    IngredientModel(name: details.strIngredient14, quantity: details.strMeasure14),
-                    IngredientModel(name: details.strIngredient15, quantity: details.strMeasure15),
-                ]
                 
-                
-                self.ingredients = ingredMeasurements.filter { !($0.name.removeEmpty() || $0.quantity.removeEmpty()) }
-            }
+            await fetch(from: detailsURL, onSuccess: handleSuccess)
+        }
             
+        private func setInstructions(using details: NetworkResponse) {
+            self.instructions = details.strInstructions
+        }
+            
+        private func setIngredients(using details: NetworkResponse) {
+            let ingredMeasurements = [
+                IngredientModel(name: details.strIngredient1, quantity: details.strMeasure1),
+                IngredientModel(name: details.strIngredient2, quantity: details.strMeasure2),
+                IngredientModel(name: details.strIngredient3, quantity: details.strMeasure3),
+                IngredientModel(name: details.strIngredient4, quantity: details.strMeasure4),
+                IngredientModel(name: details.strIngredient5, quantity: details.strMeasure5),
+                IngredientModel(name: details.strIngredient6, quantity: details.strMeasure6),
+                IngredientModel(name: details.strIngredient7, quantity: details.strMeasure7),
+                IngredientModel(name: details.strIngredient8, quantity: details.strMeasure8),
+                IngredientModel(name: details.strIngredient9, quantity: details.strMeasure9),
+                IngredientModel(name: details.strIngredient10, quantity: details.strMeasure10),
+                IngredientModel(name: details.strIngredient11, quantity: details.strMeasure11),
+                IngredientModel(name: details.strIngredient12, quantity: details.strMeasure12),
+                IngredientModel(name: details.strIngredient13, quantity: details.strMeasure13),
+                IngredientModel(name: details.strIngredient14, quantity: details.strMeasure14),
+                IngredientModel(name: details.strIngredient15, quantity: details.strMeasure15),
+            ]
+            
+            
+            self.ingredients = ingredMeasurements.filter {
+                !($0.name.removeEmpty() || $0.quantity.removeEmpty())
+            }
         }
     }
 }
---
+++ MealDB-API/ViewsExtensions/DessertListEx.swift
@@ -0,0 +1,35 @@
+//
+//  DessertListEx.swift
+//  MealDB-API
+//
+//  Created by Khang Nguyen Huynh on 5/21/23.
+//
+
+import Foundation
+
+extension DessertListView {
+    @MainActor class ViewModel: ObservableObject {
+        @Published private(set) var meals = [MealModel]()
+        
+        @Published var category = MealModelCategory.Dessert
+        @Published var changingCategory = false
+        
+        var urlString: String {
+            "https://themealdb.com/api/json/v1/1/filter.php?c=\(category)"
+        }
+        
+        func fetchMealCategory() async {
+            func handleSuccess(_ mealList: MealModelCategoryQuery) -> Void {
+                self.meals = mealList.mealModels.filter({
+                    $0.strMealThumb != nil
+                }).sorted()
+            }
+            
+            await fetch(from: urlString, onSuccess: handleSuccess)
+        }
+        
+        func setCategory(category: MealModelCategory) {
+            self.category = category
+        }
+    }
+}
